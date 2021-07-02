3f4e82a4cf38f595d017dac43cd03becb1c431e3
kimiszero <zerokim0215@gmail.com>
2021-07-02 21:26:12 +0900

    A+B

--- 입출력과 사칙연산/hello.py
+++ 입출력과 사칙연산/hello.py
@@ -8,12 +8,15 @@ print('(  /  )')
 print(' \\(__)|')
     
 # 강아지
-
 print('|\_/|')
 print('|q p|   /}')
 print('( 0 )"""\\')
 print('|"^"`    |')
 print('||_/=\\\__|')
 
-
+# A+B
+a, b = input().split()
+a = int(a)
+b = int(b)
+print(a+b)
 
