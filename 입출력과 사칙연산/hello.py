94ea0ed396d3902fcff10776a84ac8a09f6d001a
kimiszero <zerokim0215@gmail.com>
2021-07-04 22:04:01 +0900

    사칙연산2

--- 입출력과 사칙연산/hello.py
+++ 입출력과 사칙연산/hello.py
@@ -19,3 +19,10 @@ a, b = input().split()
 a = int(a)
 b = int(b)
 print(a+b, a-b, a*b, int(a/b), a%b, sep='\n')
+
+# (A+B)%C는 ((A%C) + (B%C))%C 와 같을까?
+# (A×B)%C는 ((A%C) × (B%C))%C 와 같을까?
+# 세 수 A, B, C가 주어졌을 때, 위의 네 가지 값을 구하는 프로그램을 작성하시오.
+
+A, B, C = map(int, input().split())
+print((A+B)%C, ((A%C) + (B%C))%C, (A*B)%C, ((A%C) * (B%C))%C, sep='\n')
