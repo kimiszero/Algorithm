e826fb26599b5ee1c2901434c75e3f54ae3c9c12
kimiszero <zerokim0215@gmail.com>
2021-07-12 23:19:37 +0900

    A+B-3

--- for문/gugudan.py
+++ for문/gugudan.py
@@ -1,7 +1,13 @@
 # 구구단
-N = int(input())
-if N >= 2 and N <= 9 :        
-    for i in range(1,10) :
-        print ('%d * %d = %d' %(N, i , N*i))
-else :
-    print('2부터 9까지 숫자를 입력하세요')
+# N = int(input())
+# if N >= 2 and N <= 9 :        
+#     for i in range(1,10) :
+#         print ('%d * %d = %d' %(N, i , N*i))
+# else :
+#     print('2부터 9까지 숫자를 입력하세요')
+
+# A + B -3
+T = int(input())
+for i in range(1, T+1) :
+    A, B = map(int, input().split())
+    print(A + B)
