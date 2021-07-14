8e3bdae6f6f4766c05b5a313c46c017b504758e3
kimiszero <zerokim0215@gmail.com>
2021-07-14 22:33:47 +0900

    빠른A+B

--- for문/gugudan.py
+++ for문/gugudan.py
@@ -13,9 +13,18 @@
 #     print(A + B)
 
 # 합
-n = int(input())
-sum = 0
-if n >= 1 and n <= 10000 :
-    for i in(range(1, n+1)) :
-        sum += i
-print(sum)    
+# n = int(input())
+# sum = 0
+# if n >= 1 and n <= 10000 :
+#     for i in(range(1, n+1)) :
+#         sum += i
+# print(sum)    
+
+# 빠른 A+B
+import sys
+
+T =  int(input())
+if T <= 1000000 : 
+    for i in (range(1, T+1)) :
+        a,b = map(int, sys.stdin.readline().split())
+        print(a+b)
