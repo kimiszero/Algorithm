2aaa02ed8f92416f440b8dee5e2d77ae61041bdb
kimiszero <zerokim0215@gmail.com>
2021-07-15 22:57:42 +0900

    N찍기

--- for문/gugudan.py
+++ for문/gugudan.py
@@ -21,10 +21,17 @@
 # print(sum)    
 
 # 빠른 A+B
+#import sys
+# T =  int(input())
+# if T <= 1000000 : 
+#     for i in (range(1, T+1)) :
+#         a,b = map(int, sys.stdin.readline().split())
+#         print(a+b)
+
+# N 찍기
 import sys
 
-T =  int(input())
-if T <= 1000000 : 
-    for i in (range(1, T+1)) :
-        a,b = map(int, sys.stdin.readline().split())
-        print(a+b)
+N = int(sys.stdin.readline())
+if N <= 100000 :
+   for i in (range(1, N+1)) :
+       print(i)
