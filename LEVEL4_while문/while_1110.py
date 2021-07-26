f215e6d547b9d55da030e21f2b851f4a45bfa69e
kimiszero <zerokim0215@gmail.com>
2021-07-27 00:00:13 +0900

    while문_1110

--- LEVEL4_while문/while_1110.py
+++ LEVEL4_while문/while_1110.py
@@ -1,19 +1,17 @@
 import sys
 
 N = int(sys.stdin.readline())
+old_N = N #원래 N값
 count = 0 #N의 사이클 길이
 result = 0 #수행한 연산값
 new_N = 0 #새로 만들어질 N값 
 
 while True :
     if N >= 0 and N <= 99 :
-        A = N // 10
-        B = N % 10
-        result = A + B
-        count = count + 1
+        result = N // 10 + N % 10
         new_N = (N % 10) * 10 +  result % 10
-        if new_N != N :
-            print(result + B)
-        else :
-            print(count)
+        count = count + 1 
+        N = new_N
+        if old_N == new_N :
             break
+print(count)
