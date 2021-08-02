5876df608096488be65473ef5877a4e8582e9b0d
kimiszero <zerokim0215@gmail.com>
2021-08-02 20:05:51 +0900

    array_4344

--- test.py
+++ test.py
@@ -1,6 +1,14 @@
-cnt = 0
-i_arr = []
-for i in range(10) :
-    i = int(input()) % 42
-    a = i
-    i_arr.append(a)
+C = int(input())
+
+for i in range(C) :
+    try:
+        N = list(map(int, input().split()))
+    except :
+        print('입력되지 않았습니다')
+    else :
+        avg = sum(N[1:]) / N[0]
+        cnt = 0 
+        for j in N[1:] :
+            if j > avg :
+                cnt += 1
+        print(str('%.3f' %round((cnt/N[0])*100, 3))+ '%')
