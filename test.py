1b33cd096f68854583052ca1a405349234ccf964
kimiszero <zerokim0215@gmail.com>
2021-08-03 11:52:31 +0900

    function_15596

--- test.py
+++ test.py
@@ -1,14 +1,5 @@
-C = int(input())
-
-for i in range(C) :
-    try:
-        N = list(map(int, input().split()))
-    except :
-        print('입력되지 않았습니다')
-    else :
-        avg = sum(N[1:]) / N[0]
-        cnt = 0 
-        for j in N[1:] :
-            if j > avg :
-                cnt += 1
-        print(str('%.3f' %round((cnt/N[0])*100, 3))+ '%')
+def solve(a) :
+    sum = 0
+    for i in range(a) :
+        sum += i
+    return sum
