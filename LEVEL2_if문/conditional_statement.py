5a6fcec98ccc702d835eba37a12cfb28a088602c
kimiszero <zerokim0215@gmail.com>
2021-08-14 20:09:54 +0900

    if문_1330

--- LEVEL2_if문/conditional_statement.py
+++ LEVEL2_if문/conditional_statement.py
@@ -9,6 +9,15 @@
 # else :
 #     print('==')
 
+#update_0814
+# A, B = map(int, input().split(' '))
+# if A > B :
+#     print('>')
+# elif A < B :
+#     print('<')
+# else :
+#     print('==')
+
 # 시험 점수를 입력받아 90 ~ 100점은 A, 80 ~ 89점은 B, 70 ~ 79점은 C, 60 ~ 69점은 D, 나머지 점수는 F를 출력하는 프로그램을 작성하시오.
 # A = int(input())
 # if(A < 101 and A >= 0): 
