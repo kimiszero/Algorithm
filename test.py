b18707302e0d9e9eb529b686065a19c2498a3930
kimiszero <zerokim0215@gmail.com>
2021-08-28 20:20:23 +0900

    소수구하기

--- test.py
+++ test.py
@@ -1,11 +1,17 @@
-N = int(input())
-num = 2
+M, N = map(int, input().split())
+arr=[]
 
-while True:
-    if N % num == 0:
-        N = N // num
-        print(num)
-    elif N == 1:
-        break
+for i in range(M,N+1):
+    if i == 1:
+        pass
+    elif i == 2:
+        arr.append(i)
     else:
-        num += 1
+        for j in range(2, i):
+            if i % j == 0:
+                break
+            elif j == i - 1:
+                arr.append(i)
+
+for i in arr:
+    print(i)
