084a00f4ffae5c79f52c043bd664dbabacf1686e
kimiszero <zerokim0215@gmail.com>
2021-08-31 15:16:23 +0900

    베르트랑 공준

--- LEVEL9_기본수학2/basicMath4948.py
+++ LEVEL9_기본수학2/basicMath4948.py
@@ -1,33 +1,27 @@
-import sys
 import math
 
-data = []
-
-while True:
-    n = int(sys.stdin.readline())
-    if n == 0:
-        break
+def num(num): #소수 구하는 함수
+    if num == 1: # 소수구하는 조건1. 1은 소수가 아니다.
+        return False
     else:
-        data.append(n)
+        for i in range(2, int(math.sqrt(num))+1):
+            if num%i == 0:
+                return False
+        return True
 
-# 입력 n의 범위는 1 <= n <= 123456 이므로, 2n의 값까지 y를 설정
-y = 123456 * 2
-arr = [True for _ in range(y+1)]
-arr[1] = False
+all_list = list(range(2, 246912)) #문제에서 주어진 범위 // 이 범위를 정해두지 않으면 시간초과가 난다.
+save_list = []
 
-for i in range(2, int(math.sqrt(y))+1):
-    if arr[i]:
-        j = 2
-        while i * j <= y:
-            arr[i*j] = False
-            j += 1
+for i in all_list: #주어진 범위 안에서 소수들을 찾아 저장한다.
+    if num(i):
+        save_list.append(i)
 
+num = int(input())
 
-for x in data:
-    count = 0 # data의 각 값들에 대한 count 초기화
-
-    for i in range(x+1, 2*x+1): # x초과 2x이하까지 i가 돈다
-        if arr[i]: # arr[i]가 True면 (소수이면)
-            count += 1 # 카운트에 1 추가
-
-print(count)
+while num != 0:
+    count = 0
+    for i in save_list:
+        if num < i <= num*2:
+            count += 1
+    print(count)
+    num = int(input())
--- test.py
+++ test.py
@@ -1,17 +1,27 @@
-M, N = map(int, input().split())
-arr=[]
+import math
 
-for i in range(M,N+1):
-    if i == 1:
-        pass
-    elif i == 2:
-        arr.append(i)
+def num(num): #소수 구하는 함수
+    if num == 1: # 1은 소수가 아니다.
+        return False
     else:
-        for j in range(2, i):
-            if i % j == 0:
-                break
-            elif j == i - 1:
-                arr.append(i)
+        for i in range(2, int(math.sqrt(num))+1):
+            if num%i == 0:
+                return False
+        return True
 
-for i in arr:
-    print(i)
+all_list = list(range(2, 246912)) #문제에서 주어진 범위
+save_list = []
+
+for i in all_list: #주어진 범위 안에서 소수들을 찾아 저장한다.
+    if num(i):
+        save_list.append(i)
+
+num = int(input())
+
+while num != 0:
+    count = 0
+    for i in save_list:
+        if num < i <= num*2:
+            count += 1
+    print(count)
+    num = int(input())
