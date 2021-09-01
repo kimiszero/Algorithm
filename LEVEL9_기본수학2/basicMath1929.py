078e3065ddc36cf171a48916a483b1dc0cc28ab0
kimiszero <zerokim0215@gmail.com>
2021-09-01 13:19:06 +0900

    소수구하기

--- LEVEL9_기본수학2/basicMath1929.py
+++ LEVEL9_기본수학2/basicMath1929.py
@@ -1,9 +1,11 @@
-#소수인지 검사해주는 함수 
+import math
+
+#소수인지 검사해주는 함수
 def isPrime(num):
     if num ==  1:
         return False
     else:
-        for i in range(2, int(num**0.5)+1):
+        for i in range(2, int(math.sqrt(num))+1):
             if num%i == 0:
                 return False
         return True
--- test.py
+++ test.py
@@ -1,27 +1,26 @@
 import math
 
-def isPrime(num): #소수 구하는 함수
-    if num == 1: # 소수구하는 조건1. 1은 소수가 아니다.
+def isPrime(n):
+    if n == 1:
         return False
     else:
-        for i in range(2, int(math.sqrt(num))+1):
-            if num%i == 0:
+        for i in range(2, int(math.sqrt(n)+1)):
+            if n%i == 0:
                 return False
         return True
 
-all_list = list(range(2, 246912)) #문제에서 주어진 범위 // 이 범위를 정해두지 않으면 시간초과가 난다.
+all_list = list(range(2, 246912))
 save_list = []
 
-for i in all_list: #주어진 범위 안에서 소수들을 찾아 저장한다.
+for i in all_list:
     if isPrime(i):
         save_list.append(i)
 
-num = int(input())
-
-while num != 0:
-    count = 0
+n = int(input())
+while n != 0:
+    count = 0 
     for i in save_list:
-        if num < i <= num*2:
+        if n < i <= n*2:
             count += 1
     print(count)
-    num = int(input())
+    n = int(input())
