4935b7f2d604349fbffe868d022177c6a73388cc
kimiszero <zerokim0215@gmail.com>
2021-09-02 15:19:33 +0900

    골드바흐의추측

--- LEVEL9_기본수학2/basicMath9020.py
+++ LEVEL9_기본수학2/basicMath9020.py
@@ -1,7 +1,9 @@
+#입력값은 10000까지 주어지며 0, 1번 인덱스는 False인 소수리스트를 만든다. 
 prime_list = [False, False] + [True]*10002
 
 for i in range(2, 10002):
     if prime_list[i]:
+        #소수를 구하기 위해 반복문을 돌린다. 10002보다 작지만 2의 배수들은 소수가 아니니 False값을 담는다. 
         for j in range(2*i, 10002, i):
             prime_list[j] = False
 
@@ -9,7 +11,7 @@ T = int(input()) #입력받는 테스트 케이스의 개수 T
 
 for i in range(T):
     n = int(input()) #입력받는 짝수 n
-    a = n // 2
+    a = n // 2 #입력받은 짝수의 중간값을 만들어 탐색한다.
     b = a
     while a > 0:
         if prime_list[a] and prime_list[b]:
--- test.py
+++ test.py
@@ -1,20 +1,23 @@
-prime_list = [False, False] + [True]*10002
+import sys, math 
 
-for i in range(2, 10002):
-    if prime_list[i]:
-        for j in range(2*i, 10002, i):
-            prime_list[j] = False
-
-T = int(input())
-
-for i in range(T):
-    n = int(input())
-    a = n // 2
-    b = a
-    while a > 0:
-        if prime_list[a] and prime_list[b]:
-            print(a, b)
-            break
-        else:
-            a -= 1
-            b += 1
+input = sys.stdin.readline 
+n = 1000000 
+array = [True for i in range(n + 1)] 
+prime = [] 
+for i in range(2, n + 1): 
+    if array[i] == True: 
+        j = 2 
+        prime.append(i) 
+        while i * j <= n: 
+            array[i * j] = False 
+            j += 1 
+            
+for _ in range(int(input())): 
+    n = int(input()) 
+    cnt = 0 
+    for i in range(n // 2 , 1 , -1): 
+        if array[i] == True and array[n-i] == True: 
+            cnt += 1 
+        else: 
+            continue 
+    print(cnt)
