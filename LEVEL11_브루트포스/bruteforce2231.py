9fb2a6d55ab4e9bcf7495b9d3d6834dad46cfd8f
kimiszero <zerokim0215@gmail.com>
2021-09-29 21:39:24 +0900

    분해합

--- LEVEL11_브루트포스/bruteforce2231.py
+++ LEVEL11_브루트포스/bruteforce2231.py
@@ -2,7 +2,8 @@ n = int(input()) #입력받은 자연수 n
 
 for i in range(1, n+1):
     arr = list(map(int, str(i)))
-    if sum(arr) + i == n:
+    hap = i + sum(arr)
+    if hap == n:
         print(i)
         break
     if i == n:
